1a7e4121474e7a5028d746fce1f0164dddb7c8db
kevinfukuhara <kevinfukuhara@gmail.com>
2019-06-03 12:56:56 -0700

    reformatted the cat app to for burgers.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Hamburger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Impossible Burger', false);
