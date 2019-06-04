e2cb691ca073a9a0b3dd12d623c7b73d7829118e
Fukuhara <Kevin.Fukuhara2@t-mobile.com>
2019-06-03 23:28:01 -0700

    changed connection.js for heroku DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "12345",
-  database: "burger_db2"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "12345",
+//   database: "burger_db2"
+// });
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'yaipgiybzyjoryor',
+    password:'xdgjkva5mtt98bqt',
+    database: 'v4grh6a2q84c39nu'
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
